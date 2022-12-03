178892daad71ef672c5e42eaf61ca1b241510609
viktormedk <119461108+viktormedk@users.noreply.github.com>
2022-12-03 13:21:30 +0100

    Update day2.py

--- python/day2/day2.py
+++ python/day2/day2.py
@@ -1,14 +1,34 @@
-part1 = {"AX": 4, "BX": 1, "CX": 7, "AY": 8, "BY": 5,
-         "CY": 2, "AZ": 3, "BZ": 9, "CZ": 6}
+part1 = {
+    "AX": 4,
+    "BX": 1,
+    "CX": 7,
+    "AY": 8,
+    "BY": 5,
+    "CY": 2,
+    "AZ": 3,
+    "BZ": 9,
+    "CZ": 6,
+}
 
-part2 = {"AX": 3, "BX": 1, "CX": 2, "AY": 4, "BY": 5,
-         "CY": 6, "AZ": 8, "BZ": 9, "CZ": 7}
+part2 = {
+    "AX": 3,
+    "BX": 1,
+    "CX": 2,
+    "AY": 4,
+    "BY": 5,
+    "CY": 6,
+    "AZ": 8,
+    "BZ": 9,
+    "CZ": 7,
+}
 
 
 def day2(input, score):
     result = 0
+
     for line in input:
         result += score[f"{line[0]}" + f"{line[2]}"]
+
     print(result)
 
 
@@ -18,6 +38,6 @@ def read_input(filename):
     return list(input_data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     day2(read_input("input"), part1)
     day2(read_input("input"), part2)
