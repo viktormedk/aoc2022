4175bfd27a01e84a7a57322ca8e003d584e2f8fd
viktormedk <119461108+viktormedk@users.noreply.github.com>
2022-12-03 09:52:49 +0100

    Add day2 skeleton

---
+++ python/day2/day2.py
@@ -0,0 +1,14 @@
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+def read_input(filename):
+    input_data = open(filename, "r")
+
+    return list(input_data)
+
+if __name__ == '__main__':
+    part1(read_input("tinput"))
