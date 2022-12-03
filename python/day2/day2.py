145f9224930b6f2563369a3134a216e707261a6f
viktormedk <119461108+viktormedk@users.noreply.github.com>
2022-12-03 12:07:44 +0100

    Finish day2

--- python/day2/day2.py
+++ python/day2/day2.py
@@ -1,14 +1,24 @@
-def part1(input):
-    pass
+part1 = {"AX": 4, "BX": 1, "CX": 7, "AY": 8, "BY": 5,
+         "CY": 2, "AZ": 3, "BZ": 9, "CZ": 6}
+
+part2 = {"AX": 3, "BX": 1, "CX": 2, "AY": 4, "BY": 5,
+         "CY": 6, "AZ": 8, "BZ": 9, "CZ": 7}
 
 
-def part2(input):
-    pass
+def day2(input, score):
+    result = 0
+    for line in input:
+        result += score[f"{line[0]}" + f"{line[2]}"]
+    print(result)
+
 
 def read_input(filename):
     input_data = open(filename, "r")
 
     return list(input_data)
 
+
+rules = {}
 if __name__ == '__main__':
-    part1(read_input("tinput"))
+    day2(read_input("input"), part1)
+    day2(read_input("input"), part2)
